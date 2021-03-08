db3dd619f47acdcbe698d371a009c7ae2b53edea
Volker Hilsenstein <volker.hilsenstein@embl.de>
2021-03-08 17:24:25 +0100

    changes to rundocker.sh

--- server_scripts/rundocker.sh
+++ server_scripts/rundocker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # find first available port, we don't want to use the standard 8888 jupyter port
 FREEPORT=`/usr/bin/python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'`
-echo $FREEPORT > .jupyter_docker_port
+echo $FREEPORT > $HOME/.jupyter_docker_port
 docker run --rm  \
           --gpus all \
           -p 10.11.19.188:$FREEPORT:8888 \
-          -v $HOME:/localhome \
-          -v /tmp/dockershare:/dockershare \
-          spacem/jupyter-desktop-server 2> .jupyter_docker_stderr
+          -v $HOME:/home/jovyan/readonly_$USER \
+          -v /shared/$USER:/home/jovyan/shared_$USER \
+          spacem/jupyter-desktop-server 2> $HOME/.jupyter_docker_stderr
 echo http://10.11.19.177:$FREEPORT/
 
 # Below is an attempt to change the user ID but it causese errrors sarting up the
